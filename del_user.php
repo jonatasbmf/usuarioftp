80ba5429f0d95d5e679910f814451d3657820e50
Unknown <jonatasbmf@gmail.com>
2018-02-02 15:33:41 -0200

    Mudança na listagem de usuários, Solicitação de limpesa de pasta

---
+++ del_user.php
@@ -0,0 +1,12 @@
+<?php 
+$_user = $_GET['usuario'];
+if ($_user == '' ){
+    header('Location: index.html');
+}
+    $arquivo = fopen("limpar.txt", 'a');
+    $texto = "$_user\n";
+    fwrite($arquivo, strval($texto));
+	fclose($arquivo);
+	echo "<script>alert('Sua solicitação de limpesa da pasta foi enviada para o servidor, em breve será executada')</script>";
+    echo "<script>location.href='index.html'</script>";
+?>
--- listagem.php
+++ listagem.php
@@ -1,3 +1,21 @@
+<?php 
+	function verificaSeExiste($existe){
+		 $arquivo = fopen("cad/cad_padrao.txt", "r");
+		 while (!feof($arquivo)){		 
+			 $linha = fgets($arquivo,6080);
+				$usuarioVerificado = trim($linha);
+				if (!strcmp($existe, $usuarioVerificado)){
+					return true;
+					break;
+				}
+		}
+		fclose ($arquivo);
+		return false;
+	}
+
+
+?>
+
 <Html>
 <head>
 <meta charset='utf-8'>
@@ -28,18 +46,49 @@
 			<div class="col-md-3"></div>
 				<div class="col-md-6">
 					<div class="portlet light bordered">
-						<?php
-							$arquivo = fopen("cadastrados.txt", 'r+');
-							while (!feof($arquivo)){
-								$linha = fgets($arquivo,6080);
-								echo $linha.'<br>';
-							}
-							fclose ($arquivo);
-						?>
+					<?php
+					$arquivo = fopen("cadastrados.txt", 'r+');
+					$meusUsuarios = array();
+					
+					while (!feof($arquivo)){
+						$linha = fgets($arquivo,6080);
+						$userVerificar = trim($linha);
+						if (!verificaSeExiste($userVerificar)){
+							$meusUsuarios[] = $userVerificar;
+						}		
+					}
+					fclose ($arquivo);
+					asort($meusUsuarios); //Está organizando os valores em ordem alfabética
+					$meusUsuarios = array_filter($meusUsuarios); // Está tirando todo e qualquer valor vazio ou nulo do array
+					echo '<table class="table" style="text-align:center;vertical-align:middle;">';
+						echo '<tr>';
+							echo '<th style="text-align:center;vertical-align:middle;">';
+								echo "Usuário";
+							echo "</th>";
+							echo '<th style="text-align:center;vertical-align:middle;">';
+								echo "Função";
+							echo "</th>";			
+						echo "</tr>";
+
+					echo '<tbody class="table-striped">';
+						foreach ($meusUsuarios as $_user){
+							echo '<tr>';
+									echo "<td>";
+										echo $_user;
+									echo "</td>";
+									echo "<td>";
+										echo '<a href="del_user.php?usuario='. $_user .'"><input class="btn btn-danger" type="submit" name="Gerar" Value="Limpar pasta do usuário"></input></a>';
+									echo "</td>";					
+							echo '</tr>';		
+						}			
+					echo '</tbody>';	
+					echo "</table>";
+					?>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+
 </body>
 </html>
